b827376704fc143817dfc2313edb9094a9dcbbb5
cc <claude.cockfield@bison.howard.edu>
2025-06-19 08:42:18 -0400

    adjusted NNN langth for scenario test

--- anontion/src/main/account/java/com/anontion/account/controller/AccountController.java
+++ anontion/src/main/account/java/com/anontion/account/controller/AccountController.java
@@ -213,7 +213,7 @@ public class AccountController {
         
         _count++;
         
-        String NNN = _count.toString();
+        String NNN = _count.toString() + _count.toString() + _count.toString();
 
         id = NNN;
 
