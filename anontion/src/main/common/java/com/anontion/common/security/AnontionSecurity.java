ec4a9bb56842059783716536216316001fb17f44
cc <claude.cockfield@bison.howard.edu>
2025-08-17 21:04:31 -0400

    test for safe encode

--- anontion/src/main/common/java/com/anontion/common/security/AnontionSecurity.java
+++ anontion/src/main/common/java/com/anontion/common/security/AnontionSecurity.java
@@ -178,7 +178,7 @@ public class AnontionSecurity {
       return "";
     }
     
-    return base64Encoded.replace('/', '-').replace('+', '_').replace("=", "");
+    return base64Encoded.replace('/', '1').replace('+', '1').replace("=", "");
   }
   
   public static String decodeFromSafeBase64(String safePubEncoded) {
