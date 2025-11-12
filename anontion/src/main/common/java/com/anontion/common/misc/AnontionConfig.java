1740081d9820b91553a945860159b6f588f7be7a
cc <claude.cockfield@bison.howard.edu>
2025-11-12 04:25:40 -0500

    brodcast timeout to 30 mins

--- anontion/src/main/common/java/com/anontion/common/misc/AnontionConfig.java
+++ anontion/src/main/common/java/com/anontion/common/misc/AnontionConfig.java
@@ -24,7 +24,7 @@ public class AnontionConfig {
   public static String _ASTERISK_PASSWORD_ENCODING_AUTHTYPE = "md5";
   public static String _ASTERISK_PASSWORD_ENCODING_REALM = "asterisk";
   
-  public static Long _CONTACT_CONNECTION_BROADCAST_TIMEOUT = 300L; // 5 minutes possible to make new call.
+  public static Long _CONTACT_CONNECTION_BROADCAST_TIMEOUT = 6000L; //300L; // 5 minutes possible to make new call.
   public static Long _CONTACT_CONNECTION_BROADCAST_TIMEOUT_NOTIMEOUT = 0L;
   
   public static Double _CONTACT_GPS_NULL_LONGITUDE = 1800.0;
